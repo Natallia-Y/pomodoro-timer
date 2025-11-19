26ee8c583b89b0ff291f3c9dc4e99763656d07b2
Natallia-Y <tusjabrest@mail.ru>
2025-11-19 17:57:08 +0300

    продвинутый уровень JS

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const startBtn = document.querySelector('#start');
 const timer = document.querySelector('#pomodoro-time');
+const resetBtn = document.querySelector('#reset');
+const pomodoroBtn = document.querySelector('#pomodoro');
+const breakBtn = document.querySelector('#break');
 
-let time = 1500;
+let timeInSecondsNow = 1500;
 let timerId = null;
+let nowMode = null;
+
+const timePomodoro = 1500;
+const timeBreak = 300;
 
 function clearTimer() {
   clearInterval(timerId);
@@ -11,8 +18,8 @@ function clearTimer() {
 }
 
 function updateTimer() {
-  let minutes = Math.floor(time / 60);
-  let seconds = time % 60;
+  let minutes = Math.floor(timeInSecondsNow / 60);
+  let seconds = timeInSecondsNow % 60;
 
   minutes = String(minutes).padStart(2, '0');
   seconds = String(seconds).padStart(2, '0');
@@ -20,20 +27,51 @@ function updateTimer() {
   timer.innerHTML = `${minutes}:${seconds}`;
 }
 
-startBtn.addEventListener('click', function () {
+function toggleTimer() {
   if (!timerId) {
     startBtn.textContent = "stop";
     timerId = setInterval(() => {
-      time--;
+      timeInSecondsNow--;
       updateTimer();
 
-      if (time < 0) {
+      if (timeInSecondsNow < 0) {
         clearTimer();
-        timer.innerHTML = "25:00";
-        time = 1500;
+
       }
     }, 200)
   } else {
     clearTimer();
   }
+}
+
+startBtn.addEventListener('click', toggleTimer);
+
+resetBtn.addEventListener('click', function () {
+  clearTimer();
+  if(nowMode === 'pomodoro') {
+    timeInSecondsNow = timePomodoro;
+  } else {
+    timeInSecondsNow = timeBreak;
+  }
+  updateTimer();
+})
+
+breakBtn.addEventListener('click', function () {
+  clearTimer();
+  timeInSecondsNow = timeBreak;
+  updateTimer();
+  breakBtn.classList.add('active');
+  pomodoroBtn.classList.remove('active');
+
+  nowMode = 'break';
+})
+
+pomodoroBtn.addEventListener('click', function () {
+  clearTimer();
+  timeInSecondsNow = timePomodoro;
+  updateTimer();
+  breakBtn.classList.remove('active');
+  pomodoroBtn.classList.add('active');
+
+  nowMode = 'pomodoro';
 })
